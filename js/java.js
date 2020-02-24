2a6dca28f2bafc4168dc004ad9565ea323d191ca
Andreas Hoffmann <Hoffmann@andreass-mbp-3.ucn.moc>
2020-02-24 16:46:50 +0100

    Java

---
+++ js/java.js
@@ -0,0 +1,17 @@
+
+
+
+var hide = document.getElementById('testimg');
+
+ 
+
+function sort1() {
+    alert('hello');
+    
+    
+    hide.style["display"] = "none"; 
+    hide.innerHTML = "Style Attribute Changed";
+
+};
+
+// document.getElementById('testimg'); style.display = 'none';
