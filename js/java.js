bb41e26d3a0710bfa045c59c1fd8bdae69261d2a
SimonHougi <1081555@ucn.dk>
2020-02-25 14:44:58 +0100

    Update

--- js/java.js
+++ js/java.js
@@ -5,3 +5,12 @@ function toggleList(kategori) {
       kategoriArray[i].classList.toggle("invisible");
     }
 }
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
