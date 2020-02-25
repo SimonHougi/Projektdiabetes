ada341c7fe8765058be3ff0586df551b3d8112c2
Andreas Hoffmann <Hoffmann@andreass-mbp-3.ucn.moc>
2020-02-25 15:36:28 +0100

    icon cross

--- js/java.js
+++ js/java.js
@@ -6,11 +6,21 @@ function toggleList(kategori) {
     }
 }
 
+var burger = document.getElementById("burger");
+var cross = document.getElementById("cross");
+
 function myFunction() {
   let x = document.getElementById("navLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
+    burger.style.display = "block"
+    cross.style.display = "none"
+
   } else {
     x.style.display = "block";
+    burger.style.display = "none"
+    cross.style.display = "block"
   }
 }
+
+
